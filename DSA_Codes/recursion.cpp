1a54d491c1e6c22ddda93808e50610879bb87e37
Ashish Ranjan <148757537+AshShubh72@users.noreply.github.com>
2026-06-30 10:04:09 +0530

    30 june

---
+++ DSA_Codes/recursion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void printNumbers(int n) {
+    if (n > 10) {
+        return;
+    }
+    cout << n << " ";
+    printNumbers(n + 1);
+}
+
+int main() {
+    printNumbers(1);
+    cout << "\n";
+    return 0;
+}
